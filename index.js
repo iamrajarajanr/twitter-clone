0fe4a1068c3b268e2c376d25be3f33e7e7e765b1
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-08-13 12:31:14 +0530

    ch

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ console.log(tweetsData)
 
 const tweetInput = document.getElementById("tweet-input")
 const tweetBtn = document.getElementById("tweet-btn")
+const feed = document.getElementById("feed")
 
 tweetBtn.addEventListener("click", function(){
     console.log(tweetInput.value)
@@ -22,29 +23,42 @@ tweetBtn.addEventListener("click", function(){
 function getfeedHtml(){
 
     let feedHtml=""
-    for(let tweet of tweetsData){
-     feedHtml +=`<div class="tweet">
+    tweetsData.forEach(function(tweet){
+        feedHtml +=`<div class="tweet">
     <div class="tweet-inner">
         <img src="${tweet.profilePic}" class="profile-pic">
         <div>
             <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.text}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
+                <i class="fa-regular fa-comment-dots"></i>
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
+                <i class="fa-solid fa-heart"></i>
                    ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
+                <i class="fa-solid fa-retweet"></i>
                     ${tweet.retweets}
                 </span>
             </div>
         </div>
     </div>
 </div>`
-}
-feed.innerHTML=feedHtml
+
+    })
+
+return feedHtml
 }
 
 getfeedHtml()
+
+
+function render (){
+feed.innerHTML = getfeedHtml()
+
+}
+
+render()
