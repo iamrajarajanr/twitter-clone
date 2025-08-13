d6fa2987e67d9834b797134858fa73264a70829e
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-08-13 14:16:07 +0530

    ch

--- dataAttribute/index.js
+++ dataAttribute/index.js
@@ -2,7 +2,8 @@ document.addEventListener('click', function(e){
 
         if (e.target.dataset.share){
             console.log(e.target.dataset.share)
-        }
+        }else if (e.target.dataset.heart)
+             console.log(e.target.dataset.heart)
     }
 )
 
